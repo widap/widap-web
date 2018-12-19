5fc68962cd776b60e66682964c886e82a435822c
Scott Jespersen <scott.d.jespersen@gmail.com>
2018-12-18 16:53:11 -0800

    coerce state plot generation into same form as plant-based plot generation

--- pipeline/make_overview_plots.py
+++ pipeline/make_overview_plots.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-USAGE = "Usage: python make_overview_plots.py < orispl_codes.txt"
+USAGE = "Usage: python make_overview_plots.py < <orispl_codes_file|state_abbrevs_file>"
 
-def save_gload_trend_svg(orispl_code, df):
+def save_gload_trend_svg(entity_id, df):
     dt = [np.datetime64(x) for x in df.year_month]
     plt.clf()
     plt.fill_between(dt, df.min_gload_mw, df.max_gload_mw, facecolor='gray', alpha=0.3)
@@ -17,9 +17,9 @@ def save_gload_trend_svg(orispl_code, df):
     plt.xlim(dt[0], dt[-1])
     plt.ylim(bottom=0) # let matplotlib figure out the top
     plt.tight_layout()
-    plt.savefig("../img/svg/gloadtrend/%s.svg" % orispl_code, transparent=True, bbox='tight')
+    plt.savefig("../img/svg/gloadtrend/%s.svg" % entity_id, transparent=True, bbox='tight')
 
-def save_emissions_svg(orispl_code, df):
+def save_emissions_svg(entity_id, df):
     dt = [np.datetime64(x) for x in df.year_month]
     plt.clf()
 
@@ -44,20 +44,23 @@ def save_emissions_svg(orispl_code, df):
     plt.xlabel("Date")
 
     plt.tight_layout()
-    plt.savefig("../img/svg/emissions/%s.svg" % orispl_code, transparent=True)
+    plt.savefig("../img/svg/emissions/%s.svg" % entity_id, transparent=True)
 
-def generate_plots(orispl_code):
+def generate_plots(entity_id):
     df = pd.DataFrame()
     try:
-        df = pd.read_csv("../csv/monthly/%s.csv" % orispl_code)
+        df = pd.read_csv("../csv/monthly/%s.csv" % entity_id)
     except Exception as e:
-        print("I had a problem with", orispl_code)
+        print("I had a problem with", entity_id)
         return
-    save_gload_trend_svg(orispl_code, df)
-    save_emissions_svg(orispl_code, df)
+    df.index = pd.DatetimeIndex(df.year_month)
+    # Remove 2018 data; not sure it's validated
+    pre_2018 = df.loc[df.index < '2018-01']
+    save_gload_trend_svg(entity_id, pre_2018)
+    save_emissions_svg(entity_id, pre_2018)
 
 if __name__ == '__main__':
     for line in sys.stdin:
-        orispl_code = line.strip()
-        print("Processing", orispl_code)
-        generate_plots(orispl_code)
+        orispl_code_or_state = line.strip()
+        print("Processing", orispl_code_or_state)
+        generate_plots(orispl_code_or_state)
