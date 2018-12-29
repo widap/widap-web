2df94c654a01c32d5970360502c00484634097cc
Scott Jespersen <scott.d.jespersen@gmail.com>
2018-12-28 18:04:27 -0800

    add first attempt at interactive box plot using plotly

--- js/monthly-gload-box-plot.js
+++ js/monthly-gload-box-plot.js
@@ -1,110 +1,71 @@
-function prepBoxplotData(data) {
+function prepGloadData(data) {
   ymdParser = d3.timeParse("%Y-%m-%d");
   records = []
-  for (var i = 0; i < data.index.length; i++) {
-    record = {op_date: ymdParser(data.index[i])}
-    for (var j = 0; j < data.columns.length; j++) {
-      record[data.columns[j]] = data.data[i][j]
-    }
-    records.push(record);
+  for (var i = 0; i < data.length; i++) {
+    records.push({
+      op_date: ymdParser(data[i].OP_DATE),
+      gload: parseFloat(data[i].GLOAD) * parseFloat(data[i].OP_TIME),
+    });
   }
   return records;
 }
 
-function drawBoxes(g) {
-  g.append("path")
-    .attr("fill", "none")
-    .attr("stroke-width", 0.4)
-    .attr("stroke", "#336")
-    .attr("d", d => `
-      M${x(d.x0) + 1.5},${y(d.quartiles[2])}
-      H${x(d.x1) - 0.5}
-      V${y(d.quartiles[0])}
-      H${x(d.x0) + 1.5}
-      Z
-    `);
-}
+function renderMonthlyGloadBoxPlot(data, boxplot_div_id) {
+  data = prepGloadData(data)
 
-function drawWhiskers(g) {
-  g.append("path")
-      .attr("stroke", "#336")
-      .attr("stroke-width", 0.5)
-      .attr("stroke-opacity", 0.5)
-      .attr("d", d => `
-        M${(x(d.x0) + x(d.x1) + 1) / 2}, ${y(d.range[1])}
-        V${y(d.quartiles[2])}
-      `);
-  g.append("path")
-      .attr("stroke", "#336")
-      .attr("stroke-width", 0.5)
-      .attr("stroke-opacity", 0.5)
-      .attr("d", d => `
-        M${(x(d.x0) + x(d.x1) + 1) / 2}, ${y(d.range[0])}
-        V${y(d.quartiles[0])}
-      `);
-}
-
-function drawMeans(g) {
-  g.append("path")
-    .attr("stroke", "#399")
-    .attr("stroke-width", 1.8)
-    .attr("d", d => `
-      M${x(d.x0) + 1.5},${y(d.quartiles[1])}
-      H${x(d.x1) - 0.5}
-    `);
-}
-
-function renderMonthlyGloadBoxPlot(data, boxplot_svg_id, margin) {
-  data = prepBoxplotData(data)
-  svg = d3.select(`#${boxplot_svg_id}`)
-  height = +svg.attr("height")
-  width = +svg.attr("width")
-
-  svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
   monExtent = d3.extent(data, d => d.op_date)
   startDate = d3.timeMonth.floor(monExtent[0])
   endDate = d3.timeMonth.ceil(monExtent[1])
+  monRange = d3.timeMonths(d3.timeMonth.ceil(monExtent[0]), endDate)
 
-  x = d3.scaleTime()
+  bins = d3.histogram()
     .domain([startDate, endDate])
-    .rangeRound([margin.left, width - margin.right])
+    .value(d => d.op_date)
+    .thresholds(monRange)
+    (data)
 
-  // TODO: Consider moving this math to the Python preproccessing step.
-  bins = data.map(
-    d => {
-      const iqr = d.q3_gload - d.q1_gload; // interquartile range
-      const r0 = Math.max(d.min_gload, d.q1_gload - iqr * 1.5);
-      const r1 = Math.min(d.max_gload, d.q3_gload + iqr * 1.5);
-      return {
-        x0: d3.timeMonth.floor(d.op_date),
-        x1: d3.timeMonth.ceil(d.op_date),
-        quartiles: [d.q1_gload, d.q2_gload, d.q3_gload],
-        range: [r0, r1]
-      };
+  byMonth = []
+  for (i = 0; i < bins.length; i++) {
+    y = []
+    for (j = 0; j < bins[i].length; j++) {
+      y.push(bins[i][j].gload)
+    }
+    thisMonth = d3.timeMonth.floor(bins[i].x0)
+    dateFmtOptions = {year: 'numeric', month: 'short'}
+    byMonth.push({
+      y: y,
+      name: thisMonth.toLocaleDateString('en-US', dateFmtOptions),
+      type: 'box',
+      line: {
+        color: 'rgba(128,128,128,0.8)',
+        width: 1.0,
+      },
+      fillcolor: 'rgba(128,128,140,0.3)',
+      boxpoints: false,
+      // hoverinfo: 'none',
     })
+  }
 
-  y = d3.scaleLinear()
-    .domain([d3.min(bins, d => d.range[0]), d3.max(bins, d => d.range[1])]).nice()
-    .range([height - margin.bottom, margin.top])
+  layout = {
+    font: "'Source Sans Pro', 'Open Sans', sans-serif",
+    showlegend: false,
+    yaxis: {
+      title: {
+        text: 'Generation (MW)',
+      },
+    },
+    xaxis: {
+      autorange: true,
+      rangeslider: {range: ['Jan 2001', 'Apr 2018']},
+    },
+    margin: {
+      l: 40,
+      r: 10,
+      t: 10,
+      b: 10,
+    }
+  }
 
-  const g = svg.append("g")
-    .selectAll("g")
-    .data(bins)
-    .enter().append("g");
-
-  drawBoxes(g);
-  drawWhiskers(g);
-  drawMeans(g);
-
-  // Add x-axis
-  svg.append('g')
-    .attr("transform", `translate(0, ${1 + height - margin.bottom})`)
-    .call(d3.axisBottom(x));
-
-  // Add y-axis
-  svg.append('g')
-    .attr("transform", `translate(${margin.left}, 0)`)
-    .call(d3.axisLeft(y));
+  Plotly.newPlot(boxplot_div_id, byMonth, layout, {displaylogo: false})
 }
--- js/plant-data-deep-dive.js
+++ js/plant-data-deep-dive.js
@@ -1,10 +1,7 @@
 function onPageLoad() {
-  d3.json("http://localhost:8000/web/data/overview/113.json")
-    .then(
-      d => renderMonthlyGloadBoxPlot(
-        d.monthly_gload_quartiles,
-        "monthly-boxplot",
-        {top: 20, right: 30, bottom: 30, left: 40}))
+  d3.csv("http://localhost:8000/dumps/cholla_113_unit_1.csv")
+    .then(d => renderMonthlyGloadBoxPlot(d, "monthly-gload-boxplot"))
+  // TODO: Do obvious preprocessing steps, e.g. ({carat, price}) => ({x: +carat, y: +price}))
   // render emissions time series next
 }
 
