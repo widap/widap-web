afaa76b749c3f11e6bf3406bc7e1a5d3437b214b
Scott Jespersen <sjespers@stanford.edu>
2019-02-09 22:10:07 -0800

    remove SqlSession usages from another script

--- pipeline/fetch_supplemental_plant_details.py
+++ pipeline/fetch_supplemental_plant_details.py
@@ -1,20 +1,21 @@
-import sql_session
+import config
+import mysql.connector
 import pandas as pd
 
 WIEB_STATES = ("WA", "CA", "OR", "NV", "AZ", "NM", "CO", "UT", "WY", "MT", "ID")
 TX_PLANTS = (9, 58562, 3456)
 
-def fetch_plant_capacity_and_co2(session, condition):
+def fetch_plant_capacity_and_co2(conn, condition):
     query = """
-        SELECT ORISPL_CODE, MAX(GLOAD) AS MAX_GLOAD, SUM(CO2_MASS) AS TOTAL_CO2
+        SELECT orispl_code, MAX(gload) AS max_gload, SUM(co2_mass) AS total_co2
         FROM
-            (SELECT ORISPL_CODE, OP_DATE, OP_HOUR, SUM(GLOAD) AS GLOAD, SUM(CO2_MASS) AS CO2_MASS
+            (SELECT orispl_code, op_date, op_hour, SUM(gload) AS gload, SUM(co2_mass) AS co2_mass
             FROM data
-            WHERE GLOAD > 0 AND {}
-            GROUP BY ORISPL_CODE, OP_DATE, OP_HOUR) AS byplant
-        GROUP BY ORISPL_CODE
+            WHERE gload > 0 AND {}
+            GROUP BY orispl_code, op_date, op_hour) AS byplant
+        GROUP BY orispl_code
     """.format(condition)
-    return session.execute_query(query)
+    return pd.read_sql(query, conn, index_col='orispl_code')
 
 
 def fetch_all_supplemental_plant_details():
@@ -29,13 +30,12 @@ def fetch_all_supplemental_plant_details():
     conditions = ["ORISPL_CODE IN " + str(TX_PLANTS)]
     for state in WIEB_STATES:
         conditions.append("STATE='%s'" % state)
-    session = sql_session.SqlSession()
     df = pd.DataFrame()
+    conn = mysql.connector.connect(**config.getcfg())
     for condition in conditions:
         print("On query with condition", condition)
-        df = df.append(fetch_plant_capacity_and_co2(session, condition))
-    df.index = df.orispl_code
-    return df.drop(["orispl_code"], axis=1)
+        df = df.append(fetch_plant_capacity_and_co2(conn, condition))
+    return df
 
 
 if __name__ == '__main__':
