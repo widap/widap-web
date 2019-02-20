fe24d3e7889eb447d389eecd2ab0c42a0b9ae14f
Scott Jespersen <sjespers@stanford.edu>
2019-02-19 22:14:01 -0800

    finish up reactifying

--- js/unit-level-dashboard.js
+++ js/unit-level-dashboard.js
@@ -2,8 +2,13 @@ const spin = require('./spin.js');
 const renderGenerationTimeSeries = require('./generation-time-series.js');
 const renderEmissionsTimeSeries = require('./emissions-time-series.js');
 const React = require('react');
+const Select = require('react-select').default;
 const ReactDOM = require('react-dom');
-const PlantUnitSelector = require('./plant-unit-selector.js');
+const PLANTS_UNITS = require('./plants-units.js');
+
+const PLANT_UNIT_MAP = loadPlantsUnits(PLANTS_UNITS);
+const PLANT_OPTIONS = PLANTS_UNITS.map(
+  row => ({label: row.name, value: row.orispl_code}));
 
 const GH_HOST = 'https://media.githubusercontent.com';
 const EMIS_DATA_REPO =  `${GH_HOST}/media/widap/emissions-data/master/csv`;
@@ -11,16 +16,15 @@ const EMIS_DATA_REPO =  `${GH_HOST}/media/widap/emissions-data/master/csv`;
 const GENERATION_TIME_SERIES = 'generation-time-series'
 const EMISSIONS_TIME_SERIES = 'emissions-time-series'
 const SPINNER_DIV = 'loading-spinner'
-
 const SPINNER_OPTS = {lines: 9, length: 5, width: 3, radius: 5};
 
-function clearPlots() {
-  updatePlots([])
-}
-
-function updatePlots(data) {
-  renderGenerationTimeSeries(GENERATION_TIME_SERIES, data);
-  renderEmissionsTimeSeries(EMISSIONS_TIME_SERIES, data);
+function loadPlantsUnits(rows) {
+  var plantsUnits = {};
+  rows.forEach(row => plantsUnits[row.orispl_code] = {
+    name: row.name,
+    unitIds: row.unit_ids,
+  });
+  return plantsUnits;
 }
 
 function parseTimeSeriesRow(row) {
@@ -34,24 +38,83 @@ function parseTimeSeriesRow(row) {
   }
 }
 
-function loadData(orisplCode, unitId) {
-  if (orisplCode && unitId) {
-    var spinner = new spin.Spinner(SPINNER_OPTS);
-    spinner.spin(document.getElementById(SPINNER_DIV));
-    clearPlots();
-    const sanitizedUnitId = unitId.replace('*', '')
-    const dataUri = `${EMIS_DATA_REPO}/${orisplCode}_${sanitizedUnitId}.csv`
-    d3.csv(dataUri, parseTimeSeriesRow).then(data => {
-      updatePlots(data);
-      spinner.stop();
-    });
+class DashboardControl extends React.Component {
+  state = {
+    selectedPlant: null,
+    unitOpts: [],
+    selectedUnit: null,
+  };
+
+  clearPlots = () => {
+    this.updatePlots([])
+  }
+
+  updatePlots = (data) => {
+    renderGenerationTimeSeries(GENERATION_TIME_SERIES, data);
+    renderEmissionsTimeSeries(EMISSIONS_TIME_SERIES, data);
+  }
+
+  componentDidMount() {
+    this.clearPlots();
+  }
+
+  handlePlantChange = (selected) => {
+    if (selected !== this.state.selectedPlant) {
+      this.setState({selectedPlant: selected, selectedUnit: null});
+      if (selected != null && PLANT_UNIT_MAP[selected.value]) {
+        const unitIds = PLANT_UNIT_MAP[selected.value].unitIds;
+        this.setState({unitOpts: unitIds.map(u => ({label: u, value: u}))});
+      }
+    }
+  }
+
+  handleUnitChange = (selected) => {
+    this.setState({ selectedUnit: selected });
+  }
+
+  loadData = (e) => {
+    const plant = this.state.selectedPlant, unit = this.state.selectedUnit;
+    if (plant != null && unit != null) {
+      // TODO: Don't actually reload unless the plant/unit have changed.
+      // TODO: Use a React spinner.
+      var spinner = new spin.Spinner(SPINNER_OPTS);
+      spinner.spin(document.getElementById(SPINNER_DIV));
+      this.clearPlots();
+      const sanitizedUnitId = unit.value.replace('*', '')
+      const dataUri = `${EMIS_DATA_REPO}/${plant.value}_${sanitizedUnitId}.csv`
+      d3.csv(dataUri, parseTimeSeriesRow).then(data => {
+        this.updatePlots(data);
+        spinner.stop();
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className='flex-row'>
+        <Select
+          id='plant-selector'
+          value={this.state.selectedPlant}
+          onChange={this.handlePlantChange}
+          options={PLANT_OPTIONS}
+          className='selector'
+          placeholder='Select a plant...'
+        />
+        <Select
+          id='unit-selector'
+          value={this.state.selectedUnit}
+          onChange={this.handleUnitChange}
+          options={this.state.unitOpts}
+          className='selector'
+          placeholder='Select a unit...'
+          noOptionsMessage={() => 'Select a plant first'}
+        />
+        <button id="load-data-button" onClick={this.loadData}>Load data</button>
+        <div id={SPINNER_DIV}></div>
+      </div>
+    );
   }
 }
 
-$(document).ready(function() {
-  clearPlots();
-  ReactDOM.render(
-    <PlantUnitSelector loadData={loadData} />,
-    document.getElementById('plant-unit-selector'));
-})
-
+ReactDOM.render(
+  <DashboardControl />, document.getElementById('dashboard-control'));
