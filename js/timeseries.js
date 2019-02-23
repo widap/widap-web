9538484b52479ee2c718c836e4db9202e3ab4c33
Scott Jespersen <sjespers@stanford.edu>
2019-02-22 18:41:09 -0800

    Speed up quantile computation.
    Prior to this change, we were computing histograms in an inefficient
    manner. Because we know the data is sorted by time on the way in,
    we only need to check whether we've crossed a date boundary (e.g.
    next day, next week, next month) to start a new bin rather than
    calling timeInterval#floor on every Date object, which is somewhat
    expensive. This speeds things up considerably, although ScatterGL
    is still eating up tons of time...

--- js/timeseries.js
+++ js/timeseries.js
@@ -21,55 +21,71 @@ function parseDateWithSecondResolution(spec) {
   return YMD_HMS_PARSER(spec.split('.')[0]);
 }
 
+function addDays(days) {
+  return (date) => date.setDate(date.getDate() + days);
+}
+
+function addMonths(months) {
+  return (date) => date.setMonth(date.getMonth() + months);
+}
+
 function quantilesFromBins(bins) {
-  return Object.keys(bins).map(
-    idx => {
-      const vals = bins[idx];
-      return {
-        t: +idx,
-        min: vals[0],
-        q1: quantile(vals, 0.25),
-        q2: quantile(vals, 0.5),
-        q3: quantile(vals, 0.75),
-        max: vals[vals.length - 1],
-      };
-    });
+  return bins.map(bin => ({
+    t: bin.t,
+    min: bin.values[0],
+    q1: quantile(bin.values, 0.25),
+    q2: quantile(bin.values, 0.5),
+    q3: quantile(bin.values, 0.75),
+    max: bin.values[bin.values.length - 1],
+  }));
 }
 
 function sortBinVals(bins) {
-  Object.keys(bins).forEach(idx => bins[idx].sort((a, b) => a - b));
+  bins.forEach(bin => bin.values.sort((a, b) => a - b));
+}
+
+function quantilesFromDailyBins(dailyBins, d3Interval, increment) {
+  var dateBoundary = d3Interval.floor(dailyBins[0].t);
+  var bins = [{t: new Date(dateBoundary.valueOf()), values: []}];
+  increment(dateBoundary);
+  var i = 0;
+  while (i < dailyBins.length) {
+    if (dailyBins[i].t < dateBoundary) {
+      dailyBins[i].values.forEach(v => bins[bins.length-1].values.push(v));
+      i++;
+    } else {
+      bins.push({t: new Date(dateBoundary.valueOf()), values: []});
+      increment(dateBoundary);
+    }
+  }
+  sortBinVals(bins);
+  return quantilesFromBins(bins);
 }
 
 export function getQuantiles(data, col) {
   if (data.length == 0) {
     return {daily: [], weekly: [], monthly: []};
   }
-  var dailyBins = {};
-  data.forEach(d => {
-    const day = timeDay.floor(d.datetime).getTime();
-    dailyBins[day] = dailyBins[day] || [];
-    dailyBins[day].push(d[col]);
-  });
+  var nextDay = timeDay.floor(data[0].datetime);
+  var dailyBins = [{t: new Date(nextDay.valueOf()), values: []}];
+  addDays(1)(nextDay);
+  var i = 0;
+  while (i < data.length) {
+    if (data[i].datetime < nextDay) {
+      dailyBins[dailyBins.length-1].values.push(data[i][col]);
+      i++;
+    } else {
+      dailyBins.push({t: new Date(nextDay.valueOf()), values: []});
+      addDays(1)(nextDay);
+    }
+  }
   // Sorting daily bins prior to constructing weekly and monthly bins enables us
   // to speed up sorting on those two because they will already be semi-sorted.
   sortBinVals(dailyBins);
-  var weeklyBins = [], monthlyBins = [];
-  Object.keys(dailyBins).forEach(day => {
-    const week = timeWeek.floor(day).getTime();
-    const month = timeMonth.floor(day).getTime();
-    weeklyBins[week] = weeklyBins[week] || [];
-    monthlyBins[month] = monthlyBins[month] || [];
-    dailyBins[day].forEach(v => {
-      weeklyBins[week].push(v);
-      monthlyBins[month].push(v);
-    });
-  });
-  sortBinVals(weeklyBins);
-  sortBinVals(monthlyBins);
   return {
     daily: quantilesFromBins(dailyBins),
-    weekly: quantilesFromBins(weeklyBins),
-    monthly: quantilesFromBins(monthlyBins),
+    weekly: quantilesFromDailyBins(dailyBins, timeWeek, addDays(7)),
+    monthly: quantilesFromDailyBins(dailyBins, timeMonth, addMonths(1)),
   };
 }
 
